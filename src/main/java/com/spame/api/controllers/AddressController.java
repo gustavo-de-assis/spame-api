7b4421704992bfe655313dd6601d54e30907212e
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-02-26 15:48:00 -0300

    feat: fetching addresses from db by id

--- src/main/java/com/spame/api/controllers/AddressController.java
+++ src/main/java/com/spame/api/controllers/AddressController.java
@@ -11,18 +11,24 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/addresses")
 public class AddressController {
 
   @Autowired
   private AddressRepository repository;
 
-  @GetMapping("/api/addresses")
+  @GetMapping
   public List<Address> getAllAddresses() {
     return repository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Optional<Address> findAddressById(@PathVariable Long id) {
+    return repository.findById(id);
   }
 
 }
