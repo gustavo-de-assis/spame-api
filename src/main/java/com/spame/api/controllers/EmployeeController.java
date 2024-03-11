f5b546be240917cf2459aa8c7e9af7ae01c63042
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-03-11 12:59:30 -0300

    feat: inserting employees on db

--- src/main/java/com/spame/api/controllers/EmployeeController.java
+++ src/main/java/com/spame/api/controllers/EmployeeController.java
@@ -1,10 +1,35 @@
 package com.spame.api.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spame.api.dtos.EmployeeDTO;
+import com.spame.api.models.Employee;
+import com.spame.api.repositories.EmployeeRepository;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/employees")
 public class EmployeeController {
+
+  @Autowired
+  private EmployeeRepository repository;
+
+  @PostMapping
+  public void create(@RequestBody @Valid EmployeeDTO req) {
+
+    repository.save(new Employee(req));
+  }
 
 }
