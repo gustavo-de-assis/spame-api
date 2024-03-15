e56953111757ddaae75af9f0b4d1708a8da89d64
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-03-15 12:41:25 -0300

    feat: authorization service

--- src/main/java/com/spame/api/services/AuthorizationService.java
+++ src/main/java/com/spame/api/services/AuthorizationService.java
@@ -1,12 +1,18 @@
 package com.spame.api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.spame.api.repositories.EmployeeRepository;
+
 @Service
 public class AuthorizationService implements UserDetailsService {
+
+  @Autowired
+  EmployeeRepository repository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
