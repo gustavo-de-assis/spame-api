1a7709892395b5c5edd074bd5988547f47896b6b
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-03-20 16:13:29 -0300

    feat: jwt token authorization on routes

--- src/main/java/com/spame/api/config/SecurityConfigurations.java
+++ src/main/java/com/spame/api/config/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.spame.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+  @Autowired
+  SecurityFilter securityFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -25,7 +29,7 @@ public class SecurityConfigurations {
             .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST, "api/employees").hasRole("ADMIN")
             .anyRequest().authenticated())
-
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
--- src/main/java/com/spame/api/controllers/AuthenticationController.java
+++ src/main/java/com/spame/api/controllers/AuthenticationController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spame.api.dtos.AuthenticationDTO;
+import com.spame.api.dtos.LoginResponseDTO;
+import com.spame.api.models.Employee;
+import com.spame.api.services.TokenService;
 
 import jakarta.validation.Valid;
 
@@ -21,12 +24,17 @@ public class AuthenticationController {
   @Autowired
   private AuthenticationManager authenticationManager;
 
+  @Autowired
+  private TokenService tokenService;
+
   @PostMapping("/login")
   public ResponseEntity login(@RequestBody @Valid AuthenticationDTO req) {
     var userNamePassword = new UsernamePasswordAuthenticationToken(req.cpf(), req.password());
     var auth = this.authenticationManager.authenticate(userNamePassword);
 
-    return ResponseEntity.ok().build();
+    var token = tokenService.generateToken((Employee) auth.getPrincipal());
+
+    return ResponseEntity.ok(new LoginResponseDTO(token));
   }
 
 }
