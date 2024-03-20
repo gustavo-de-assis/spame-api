2488bfacf5ee8c8867a14618d13e6b2d1db4e9d4
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-03-20 14:18:02 -0300

    feat: token service

---
+++ src/main/java/com/spame/api/services/TokenService.java
@@ -0,0 +1,52 @@
+package com.spame.api.services;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.spame.api.models.Employee;
+
+@Service
+public class TokenService {
+  @Value("${api.security.token.secret}")
+  private String secret;
+
+  public String generateToken(Employee employee) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      String token = JWT.create()
+          .withIssuer("spame-api")
+          .withSubject(employee.getCpf())
+          .withExpiresAt(generateExpirationDate())
+          .sign(algorithm);
+      return token;
+    } catch (JWTCreationException e) {
+      throw new RuntimeException("Error when generating token", e);
+    }
+
+  }
+
+  public String validateToken(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      return JWT.require(algorithm)
+          .withIssuer("spame-api")
+          .build()
+          .verify(token)
+          .getSubject();
+    } catch (JWTVerificationException e) {
+      return "";
+    }
+  }
+
+  private Instant generateExpirationDate() {
+    return LocalDateTime.now().plusHours(8).toInstant(ZoneOffset.of("-03:00"));
+  }
+}
