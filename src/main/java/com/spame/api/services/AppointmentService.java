eb89e83a41c1e8bf6e87eb417da1b67a4211cec9
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-03-21 11:57:39 -0300

    feat: medical record model

--- src/main/java/com/spame/api/services/AppointmentService.java
+++ src/main/java/com/spame/api/services/AppointmentService.java
@@ -6,13 +6,20 @@ import org.springframework.stereotype.Service;
 
 import com.spame.api.models.Appointment;
 import com.spame.api.repositories.AppointmentRepository;
+import com.spame.api.repositories.DoctorRepository;
+import com.spame.api.repositories.PatientRepository;
 
 @Service
 public class AppointmentService {
   final AppointmentRepository appointmentRepository;
+  final PatientRepository patientRepository;
+  final DoctorRepository doctorRepository;
 
-  AppointmentService(AppointmentRepository appointmentRepository) {
+  AppointmentService(AppointmentRepository appointmentRepository, PatientRepository patientRepository,
+      DoctorRepository doctorRepository) {
     this.appointmentRepository = appointmentRepository;
+    this.patientRepository = patientRepository;
+    this.doctorRepository = doctorRepository;
   }
 
   public Optional<Appointment> getAppointmentById(Long id) {
